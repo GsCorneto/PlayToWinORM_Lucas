77720317256836e31b1bfbc4021bd8ab00b525c9
GsCorneto <guilherme.corneto@gmail.com>
2024-03-28 11:10:02 -0400

    Formulário já craido e criação da porta

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 require("doteenv").config();
 const conn = require("./db/conn");
 
+const Usuario = require("./models/usuario")
+
+const express = require("express");
+const app = express();
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.use(express.json());
+
+app.length("/usuarios/novo", (req,res) =>{
+    res.sendFile(`${__dirname}/views/formUsuario.html`);
+});
+
+
 conn 
- .authenticate()
+ .sync()
  .then(() => {
     console.log("Conectado! :)");
  })
