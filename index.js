8d358c624e871d0d6f1f4c895b5f7c5c6b2200fd
GsCorneto <131817217+GsCorneto@users.noreply.github.com>
2024-06-10 17:23:29 -0400

    Primeiro commit, após meses de procastinação

--- index.js
+++ index.js
@@ -1,21 +1,63 @@
-require("doteenv").config();
+require("dotenv").config();
 const conn = require("./db/conn");
 
-const Usuario = require("./models/usuario")
+const Usuario = require("./models/usuario");
 
 const express = require("express");
+const handlebars = require("express-handlebars");
+
+
 const app = express();
+
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", 'handlebars')
 
 app.use(
     express.urlencoded({
-        extended: true,
+        urlencoded: true,
     })
 );
+
 app.use(express.json());
 
-app.length("/usuarios/novo", (req,res) =>{
-    res.sendFile(`${__dirname}/views/formUsuario.html`);
+app.get("/usuarios/novo", (req,res) =>{
+    res.render(`formUsuario`);
 });
+
+app.get("/", (req,res) =>{
+    res.render(`home`);
+});
+
+app.get("/usuarios", async (req,res) =>{
+    const usuarios =  await Usuario.findAll({raw: true})
+    res.render(`usuarios`, { usuarios });
+});
+
+app.post("/usuarios/novo",  async (req,res) =>{
+   const nickname = req.body.nickname; 
+   const nome = req.body.nome;
+
+   const dadosUsuario = {
+    nickname,
+    nome,
+   };
+
+   const usuario = await Usuario.create(dadosUsuario);
+
+   res.send("Usuário inserido sob o id " + usuario.id)
+});
+
+app.get("/usuario/:id/atualizar", (req,res) => 
+{
+    const id = req.params.id;
+    const usuario = Usuario.findByPk(id, {raw: true});
+    res.render("formUsuario", { usuario })
+});
+
+
+app.listen(8000, () => {
+    console.log("Server rodando na porta 8000")
+})
 
 
 conn 
